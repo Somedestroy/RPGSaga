87ef942a00b74f0c83f1dffa89143f49270e9ce9
Maxim Fomichev <maxim.fomichev@rp.ru>
2022-02-14 15:16:31 +0300

    Give another solution for this exercise

--- codewars/Convert string to camel case/ConvertStringToCamelCase.cs
+++ codewars/Convert string to camel case/ConvertStringToCamelCase.cs
@@ -2,12 +2,10 @@ public class CovertStringToCamelCase
 {
     public static string ToCamelCase(string str)
     {
-        var sb = new StringBuilder();
-        for (int i = 0; i < str.Length; i++)
-        {
-            if (str[i] != '-' && str[i] != '_') sb.Append(str[i]);
-            else sb.Append(char.ToUpper(str[++i]));
-        }
-    return sb.ToString();   
-    }
+        if (str.Length == 1) return str;
+        var words = str.Split('_', '-');
+        var startWord = words[0];
+        var endWords = words.Skip(1).Select(word => char.ToUpper(word[0]) + word.Substring(1)).ToArray();
+        return $"{startWord}{string.Join(string.Empty, endWords)}";
+  }
 }
