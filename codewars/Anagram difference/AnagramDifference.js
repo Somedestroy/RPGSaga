54836d30da1f04de74390bcc8c1fc902ce56f669
Maxim Fomichev <maxim.fomichev@rp.ru>
2022-02-15 15:27:02 +0300

    Added constants to replace magic numbers

--- codewars/Anagram difference/AnagramDifference.js
+++ codewars/Anagram difference/AnagramDifference.js
@@ -1,16 +1,18 @@
 function anagramDifference(w1,w2){
-    let count1 = Array(26).fill(0) 
-    let count2 = Array(26).fill(0) 
+    const alphabetCount = 26
+    const alphabetStartIndex = 97;
+    let count1 = Array(alphabetCount).fill(0) 
+    let count2 = Array(alphabetCount).fill(0) 
     let result = 0;
     for (let i = 0; i < w1.length; i++)
       {
-        count1[w1[i].charCodeAt()-97] += 1
+        count1[w1[i].charCodeAt()-alphabetStartIndex] += 1
       }
     for (let i = 0; i < w2.length; i++)
       {
-        count2[w2[i].charCodeAt()-97] += 1
+        count2[w2[i].charCodeAt()-alphabetStartIndex] += 1
       }
-    for (let i = 0; i < 26; i++) {
+    for (let i = 0; i < alphabetCount; i++) {
         result += Math.abs(count1[i] - count2[i]) 
     }
     return result 
