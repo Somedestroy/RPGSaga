0907736f711a86c188a7216fdb28c9ecc435d11a
Maxim Fomichev <maxim.fomichev@rp.ru>
2022-02-15 10:43:06 +0300

    Improve existing code

--- codewars/Where my anagrams at/Anagrams.cs
+++ codewars/Where my anagrams at/Anagrams.cs
@@ -3,9 +3,10 @@ public class Anagrams
     public List<string> Anagrams(string word, List<string> words)
     {
         List<string> result = new List<string>();
+        string orderedWord = string.Concat(word.OrderBy(b => b));
         foreach (var item in words)
         {
-        if (string.Concat(item.OrderBy(c => c)) == string.Concat(word.OrderBy(b => b)))
+        if (orderedWord == string.Concat(item.OrderBy(c => c)))
           {
             result.Add(item);
           }
