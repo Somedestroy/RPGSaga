f17b6077ed7fc3ae40c155d63c05f1c7fa0a14ae
Maxim Fomichev <maxim.fomichev@rp.ru>
2022-02-15 10:20:04 +0300

    Solve merge conflict

--- codewars/Sum of Digits - Digital Root/FindDigitalRoot.cs
+++ codewars/Sum of Digits - Digital Root/FindDigitalRoot.cs
@@ -2,15 +2,9 @@ public class FindDigitalRoot
 {
     public int DigitalRoot(long n)
     {
-<<<<<<< HEAD
         if ((n /10) == 0) return n;
         long sum = 0;
         while (n != 0)
-=======
-        if (n < 10) return (int)n;
-        long sum = 0;
-        while (n > 0)
->>>>>>> 1495b2d44133d48b4ed18d81b2bbeefe0a5c46d1
         {
             sum += n % 10;
             n /= 10;
