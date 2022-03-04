cfcfdd304661e284e18896a19146e68121ab3928
Maxim Fomichev <maxim.fomichev@ua.akvelon.com>
2022-03-04 17:03:41 +0300

    Fixed pipeline

--- onlineshop/server/Domain/Models/Product.cs
+++ onlineshop/server/Domain/Models/Product.cs
@@ -12,7 +12,7 @@
 
         public int CategoryId { get; set; }
 
-        public Category Category { get; set; }
+        public string Category { get; set; }
 
         public int Width { get; set; }
 
