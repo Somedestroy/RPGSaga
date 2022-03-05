25150b551edde142fcdda56207c3504c93069af5
Maxim Fomichev <maxim.fomichev@ua.akvelon.com>
2022-03-05 10:04:41 +0300

    Added attrubites for columns

--- onlineshop/server/Domain/Models/Product.cs
+++ onlineshop/server/Domain/Models/Product.cs
@@ -1,25 +1,36 @@
 ﻿namespace Domain.Models
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class Product
     {
+        [Required]
         public Guid Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Producer { get; set; }
 
+        [Required]
         public int CategoryId { get; set; }
 
+        [Required]
         public string Category { get; set; }
 
+        [Required]
         public int Width { get; set; }
 
+        [Required]
         public int Profile { get; set; }
 
+        [Required]
         public int Diameter { get; set; }
 
+        [Column(TypeName = "decimal(18,4)")]
         public decimal Price { get; set; }
     }
 }
